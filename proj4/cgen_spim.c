f3fda0fbc3099f017bffc51e9bd56c26b78dc6de
kagong <yun8755975@gmail.com>
2019-06-24 02:03:59 -0700

    add cgen for spim

---
+++ proj4/cgen_spim.c
@@ -0,0 +1,95 @@
+#include "globals.h"
+#include "cgen.h"
+
+
+
+static void genDec(TreeNode * tree){
+	TreeNode *p1, *p2, *p3;
+	int savedLoc1, savedLoc2, currentLoc;
+	int loc;
+	switch(tree->kind.decl){
+		case FunK:
+			
+			break;
+		case VarK:
+			break;
+		case VarArrK:
+			break;
+		case ParaK:
+			break;
+	}
+}
+static void genStmt(TreeNode * tree){
+    TreeNode *p1, *p2, *p3;
+    switch(tree->kind.stmt){
+        case FompndK:
+			break;
+		case CompndK:
+			break;
+		case SelcK:
+			break;
+		case IterK:
+			break;
+		case RetK:
+			break;
+		case CallK:
+			break;
+        default:
+            break;
+    }
+} /* genStmt */
+
+/* procedure genExp generates codeat an expression node */
+static void genExp( TreeNode * tree){
+    int loc;
+    TreeNode * p1, * p2;
+    switch (tree->kind.exp){
+        case ConstK:
+
+        case IdK:
+
+        case OpK:
+
+        default:
+            break;
+    }
+} /* genExp */
+
+/* procedure cGen recursively generates code by
+ * tree traversal
+ */
+static void cGen( TreeNode * tree){
+    if (tree != NULL){ 
+        switch (tree->nodekind){
+            case DeclK:
+				genDec(tree);
+				break;
+			case StmtK:
+                genStmt(tree);
+                break;
+            case ExpK:
+                genExp(tree);
+                break;
+            default:
+                break;
+        }
+        cGen(tree->sibling);
+    }
+}
+
+/***********************************************/
+/* the primary function of the code generator */
+/***********************************************/
+/* procedure codeGen generates code to a code
+ * file by traversal of .the syntax tree. The
+ * second parameter (codéfile) is the file name
+ * of the code file, and is used to print the
+ * file name as a comment in the code file
+ */
+void codeGen(TreeNode * syntaxTree, char * codefile){
+    char* s = malloc(strlen(codefile)+7);
+    strcpy(s, "File: ");
+    strcat(s,codefile);
+    cGen(syntaxTree);
+}
+
