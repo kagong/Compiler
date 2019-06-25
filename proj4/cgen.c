9526cf024daec403ca2986e8fda7f85aa591d5e7
kagong <yun8755975@gmail.com>
2019-06-25 00:18:30 -0700

    mistery bug fix

--- proj4/cgen.c
+++ proj4/cgen.c
@@ -180,7 +180,7 @@ static void genExp( TreeNode * tree){
                 fprintf(code,"\tlw $t0,%d($t1)\n",0);	
             }
             else{
-                fprintf(code,"\t%s $t0,%d(%s)\n",tree->type == Array ? "la" : "lw",tree->loc, tree->isglobal ? "$gp" : "$fp");
+                fprintf(code,"\t%s $t0,%d(%s)\n#this",tree->type == Array ? "lw" : "lw",tree->loc, tree->isglobal ? "$gp" : "$fp");
             }
             break;
         case OpK:
